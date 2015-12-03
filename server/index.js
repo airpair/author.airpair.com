0498cebb7cc312dc003e8367f2cb4d283a440f25
Jonathon Kresner <jkresner@gmail.com>
2015-12-03 21:41:38 +1100

    Few smaller changes due to fooling with meanair-server

--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
-var env = process.env.ENV || 'dev'
+var env       = process.env.ENV || 'dev'
+
 
 //-- API docs & examples (soon :p) at => airpair.com/meanair/config
-var overrides = require('./app.json')
-var envFile   = require('path').join(__dirname,`app.${env}.env`)
-var {config}  = require('meanair-server').Setup(env, overrides, envFile)
+var Config    = require('./app.json')
+var dotEnv    = require('path').join(__dirname,`app.${env}.env`)
+var {config}  = require('meanair-server').Setup(Config, env, dotEnv)
 
 
 require('./app').run(config, e => e ? $log('APP.ERROR'.red, e) : '')
